1e8314ebca4eab2c4fcddb6248975b8ee1ce7d95
mitziweb.com <samsa2001@gmail.com>
2022-11-05 11:18:34 +0100

    fix vitemanifest

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -34,7 +34,7 @@
     <div id="app"></div>
 
 
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/vue/main.js'])
 
 
 </body>
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/vue/main.js',
             ],
             refresh: true,
         }),
