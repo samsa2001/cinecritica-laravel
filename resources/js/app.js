489e3c195c771e8a71c32351b3136eed4642cc6d
mitziweb.com <samsa2001@gmail.com>
2022-11-05 11:12:24 +0100

    fix vitemanifest

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,37 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createApp } from "vue";
+
+import App from "./vue/App.vue"
+
+import axios from 'axios'
+
+import router from './vue/router'
+
+import store from './vue/store'
+
+import VueCookies from 'vue3-cookies'
+
+//tailwind
+import '../css/app.css'
+
+// oruga
+import Oruga from '@oruga-ui/oruga-next'
+import '@oruga-ui/oruga-next/dist/oruga.css'
+import '@oruga-ui/oruga-next/dist/oruga-full.css'
+
+// material design
+import "@mdi/font/css/materialdesignicons.min.css"
+
+
+const app = createApp(App)
+    .use(Oruga)
+    .use(router)
+    .use(store)
+    .use(VueCookies)
+app.config.globalProperties.$axios = axios
+window.axios = axios
+app.mount('#app')
+//createApp(App).use(Oruga).mount("#app")
