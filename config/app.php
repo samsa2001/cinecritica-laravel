63621bd34dc1f4a852833f2cb5c4249ca0a05132
mitziweb.com <samsa2001@gmail.com>
2022-11-08 19:25:50 +0100

    config

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://cine.cabogataalmeria.com'),
+    'url' => env('APP_URL', 'https://bricorodriguez.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
