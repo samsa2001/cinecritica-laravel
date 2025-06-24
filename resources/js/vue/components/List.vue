717afaab15b3f11c513bc26eb0d0a82905f15609
mitziweb.com <samsa2001@gmail.com>
2025-06-24 11:10:12 +0200

    cambios

--- resources/js/vue/components/List.vue
+++ resources/js/vue/components/List.vue
@@ -1,5 +1,9 @@
 <template>
     <div>  
+      <h1>Estrenos</h1>
+        <Carrousel :estrenos="estrenos"></Carrousel>
+    </div>
+    <div>  
       <h1>Lo más popular ahora</h1>
         <Carrousel :peliculas="peliculas" :series="series" :personas="personas"></Carrousel>
     </div>
@@ -13,6 +17,7 @@ export default  {
       peliculas:[],
       series:[],
       personas:[],
+      estrenos:[],
       isLoading: true,
     };
   },
@@ -26,7 +31,7 @@ export default  {
     listPage() {
       this.isLoading = true;
       this.$axios
-        .get("/api/peliculas")
+        .get("/api/peliculas/soloPeliculas")
         .then((res) => { 
           this.peliculas = res.data.data;      
           this.isLoading = false;
@@ -41,6 +46,12 @@ export default  {
         .get("/api/personas/soloPersonas")
         .then((res) => { 
           this.personas = res.data.data;      
+          this.isLoading = false;
+        });
+      this.$axios
+        .get("/api/peliculas")
+        .then((res) => { 
+          this.estrenos = res.data.data;      
           this.isLoading = false;
         });
     },
